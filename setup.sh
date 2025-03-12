31c2efefda1dc9ef58f6381efb6edaee2b97e654
feelware <stevanpersonal@gmail.com>
2025-03-12 13:35:39 -0500

    feat(setup.sh): add glow package

--- setup.sh
+++ setup.sh
@@ -61,3 +61,8 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/power
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow -y
